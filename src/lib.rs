3620fd0ce0f6634585f9dff0f901f0889372107a
Sebastian Goll <goll@hmi-project.com>
2023-12-19 14:10:26 +0100

    Add missing type aliases for logger callbacks

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
+use core::ffi;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 // For some reason, `bindgen` generates different type signatures with variadic arguments. We try to
@@ -14,3 +16,18 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 pub type va_list_ = *mut crate::__va_list_tag;
 #[cfg(not(all(unix, target_arch = "x86_64")))]
 pub type va_list_ = crate::va_list;
+
+/// Callback type used for [`UA_Logger::log`].
+pub type UA_LoggerLogCallback_ = Option<
+    unsafe extern "C" fn(
+        logContext: *mut ffi::c_void,
+        level: crate::UA_LogLevel,
+        category: crate::UA_LogCategory,
+        msg: *const ffi::c_char,
+        // Use unified type from above.
+        args: crate::va_list_,
+    ),
+>;
+
+/// Callback type used for [`UA_Logger::clear`].
+pub type UA_LoggerClearCallback_ = Option<unsafe extern "C" fn(context: *mut ffi::c_void)>;
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,7 @@ use core::{ffi, ptr};
 
 use open62541_sys::{
     va_list_, UA_Client_delete, UA_Client_new, UA_LogCategory, UA_LogLevel, UA_Logger,
+    UA_LoggerClearCallback_, UA_LoggerLogCallback_,
 };
 
 #[test]
@@ -25,5 +26,26 @@ fn variadic_arguments() {
         log: Some(log_c),
         context: ptr::null_mut(),
         clear: None,
+    };
+}
+
+#[test]
+fn logger_types() {
+    // Check validity of type aliases for `UA_Logger` callbacks.
+    unsafe extern "C" fn log_c(
+        _log_context: *mut ffi::c_void,
+        _level: UA_LogLevel,
+        _category: UA_LogCategory,
+        _msg: *const ffi::c_char,
+        _args: va_list_,
+    ) {
+    }
+    unsafe extern "C" fn clear_c(_context: *mut ffi::c_void) {}
+    let log: UA_LoggerLogCallback_ = Some(log_c);
+    let clear: UA_LoggerClearCallback_ = Some(clear_c);
+    let _logger = UA_Logger {
+        log,
+        context: ptr::null_mut(),
+        clear,
     };
 }
