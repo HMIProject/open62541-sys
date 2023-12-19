f2850f34167e30895041cb1aac079d588734e1ab
Sebastian Goll <goll@hmi-project.com>
2023-12-19 13:58:17 +0100

    Provide `va_list_` helper type

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,10 @@
 #![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+// For some reason, `bindgen` generates different type signatures with variadic arguments. We try to
+// export a single type that can be used instead, e.g. in `UA_Logger::log`.
+#[cfg(all(unix, target_arch = "x86_64"))]
+pub type va_list_ = *mut crate::__va_list_tag;
+#[cfg(not(all(unix, target_arch = "x86_64")))]
+pub type va_list_ = crate::va_list;
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,29 @@
-use open62541_sys::{UA_Client_delete, UA_Client_new};
+use core::{ffi, ptr};
+
+use open62541_sys::{
+    va_list_, UA_Client_delete, UA_Client_new, UA_LogCategory, UA_LogLevel, UA_Logger,
+};
 
 #[test]
 fn create_and_destroy_client() {
     let client = unsafe { UA_Client_new() };
     unsafe { UA_Client_delete(client) };
+}
+
+#[test]
+fn variadic_arguments() {
+    // Check if `va_list_` type matches.
+    unsafe extern "C" fn log_c(
+        _log_context: *mut ffi::c_void,
+        _level: UA_LogLevel,
+        _category: UA_LogCategory,
+        _msg: *const ffi::c_char,
+        _args: va_list_,
+    ) {
+    }
+    let _logger = UA_Logger {
+        log: Some(log_c),
+        context: ptr::null_mut(),
+        clear: None,
+    };
 }
