6e8f980875e8fe9fc546aeb6ed9f7221b33dfa9d
Sebastian Goll <goll@hmi-project.com>
2024-03-22 17:42:35 +0100

    Silence clippy warnings in auto-generated bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // Disable several lints. The auto-generated bindings do not conform to them.
 #![allow(clippy::missing_safety_doc)]
 #![allow(clippy::transmute_int_to_bool)]
+#![allow(clippy::type_complexity)]
 #![allow(clippy::useless_transmute)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
