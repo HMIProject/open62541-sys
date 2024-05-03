6fc8b8072d53e8e75c986707c93a84e0a391d99a
Sebastian Goll <1277035+sgoll@users.noreply.github.com>
2024-05-03 18:33:51 +0200

    Enable lints (#24)
    ## Description
    
    This PR enables lints, primarily for our own code in `build.rs` and
    `lib.rs`. The auto-generated bindings by `bindgen` are still excluded
    from the linter rules, obviously.

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,26 @@
 //!
 //! [`open62541`]: https://crates.io/crates/open62541
 
-// Disable several lints. The auto-generated bindings do not conform to them.
-#![allow(clippy::missing_safety_doc)]
-#![allow(clippy::transmute_int_to_bool)]
-#![allow(clippy::type_complexity)]
-#![allow(clippy::useless_transmute)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
+mod bindings;
 
 use core::ffi;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub use crate::bindings::*;
 
 // For some reason, `bindgen` generates different type signatures with variadic arguments. We try to
 // export a single type that can be used instead, e.g. in `UA_Logger::log`.
 #[cfg(all(unix, target_arch = "x86_64"))]
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type va_list_ = *mut crate::__va_list_tag;
 #[cfg(not(all(unix, target_arch = "x86_64")))]
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type va_list_ = crate::va_list;
 
 /// Callback type used for [`UA_Logger::log`].
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type UA_LoggerLogCallback_ = Option<
     unsafe extern "C" fn(
         logContext: *mut ffi::c_void,
@@ -37,4 +36,6 @@ pub type UA_LoggerLogCallback_ = Option<
 >;
 
 /// Callback type used for [`UA_Logger::clear`].
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type UA_LoggerClearCallback_ = Option<unsafe extern "C" fn(logger: *mut UA_Logger)>;
