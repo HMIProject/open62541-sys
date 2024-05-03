db0a38268efc3d1ee57d91fdc8c00982ade84fd6
Sebastian Goll <goll@hmi-project.com>
2024-05-03 17:08:10 +0200

    Hide non-public, non-stable helper exports from docs

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,17 @@ pub use crate::bindings::*;
 // For some reason, `bindgen` generates different type signatures with variadic arguments. We try to
 // export a single type that can be used instead, e.g. in `UA_Logger::log`.
 #[cfg(all(unix, target_arch = "x86_64"))]
-#[allow(non_camel_case_types)] // Match FFI types.
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type va_list_ = *mut crate::__va_list_tag;
 #[cfg(not(all(unix, target_arch = "x86_64")))]
-#[allow(non_camel_case_types)] // Match FFI types.
+#[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type va_list_ = crate::va_list;
 
 /// Callback type used for [`UA_Logger::log`].
 #[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type UA_LoggerLogCallback_ = Option<
     unsafe extern "C" fn(
         logContext: *mut ffi::c_void,
@@ -34,4 +37,5 @@ pub type UA_LoggerLogCallback_ = Option<
 
 /// Callback type used for [`UA_Logger::clear`].
 #[allow(non_camel_case_types)] // Match open62541 type.
+#[doc(hidden)] // Not part of stable, public crate API.
 pub type UA_LoggerClearCallback_ = Option<unsafe extern "C" fn(logger: *mut UA_Logger)>;
