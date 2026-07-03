a6e40633feec063c466c5ba5567c0445a46e56d2
Uwe Klotz <uwe.klotz@gmail.com>
2026-07-03 10:34:12 +0200

    Fix build.rs

--- build.rs
+++ build.rs
@@ -261,7 +261,7 @@ fn build_open62541(src: PathBuf, encryption: Option<&EncryptionDst>) -> PathBuf 
         // This causes libopen62541.a to be compiled with ASAN+UBSAN instrumentation. The Rust toolchain then
         // fails to link the test binary because the Apple Clang sanitizer runtime (libclang_rt.asan_osx_dynamic.dylib etc.)
         // is not linked.
-        .define("UA_ENABLE_DEBUG_SANITIZER", "OFF");
+        .define("UA_ENABLE_DEBUG_SANITIZER", "OFF")
         // Python defaults to creating bytecode in `__pycache__` directories. During build, this may
         // happen when the tool `nodeset_compiler` is called. When we package a crate, builds should
         // never modify files outside of `OUT_DIR`, so we disable the cache to prevent this.
